a36fd70232775eb0429ac6048200ec5469ed629c
Danielid Ramirez <danielid132@gmail.com>
2021-07-24 00:43:57 -0400

    Modifying wines index

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,4 +3,12 @@ class Wine < ApplicationRecord
     has_many :strains, through: :wines_strains
     validates :name, presence: true, uniqueness: true
     accepts_nested_attributes_for :wines_strains, reject_if: :all_blank, allow_destroy: true
+
+    def find_proportion(w, s)
+        join = WinesStrain.find_by(wine_id: w.id, strain_id: s.id)
+        float = join.proportion
+        (float == float.floor) ? float.to_i : float
+    end
+    
+    
 end
