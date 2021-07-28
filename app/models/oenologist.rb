a22f8d3aea29a8dacc4504b0a8ca6a8e71b4bfa6
Danielid Ramirez <danielid132@gmail.com>
2021-07-27 22:16:34 -0400

    Creating magazine model and relations

--- app/models/oenologist.rb
+++ app/models/oenologist.rb
@@ -2,4 +2,5 @@ class Oenologist < ApplicationRecord
     has_many :wines_score, dependent: :destroy
     has_many :wines, through: :wines_score
     has_and_belongs_to_many :positions
+    has_and_belongs_to_many :magazines
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_014033) do
+ActiveRecord::Schema.define(version: 2021_07_28_020612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "magazines", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "magazines_oenologists", id: false, force: :cascade do |t|
+    t.bigint "oenologist_id", null: false
+    t.bigint "magazine_id", null: false
+    t.index ["magazine_id", "oenologist_id"], name: "index_magazines_oenologists_on_magazine_id_and_oenologist_id"
+    t.index ["oenologist_id", "magazine_id"], name: "index_magazines_oenologists_on_oenologist_id_and_magazine_id"
+  end
 
   create_table "oenologists", force: :cascade do |t|
     t.string "name"
