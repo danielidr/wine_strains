44a3913489671e9412ddc81691798ddfd9d97ae9
Danielid Ramirez <danielid132@gmail.com>
2021-07-31 16:23:02 -0400

    Wine controller test

--- spec/requests/wines_spec.rb
+++ spec/requests/wines_spec.rb
@@ -2,23 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "Wines", type: :request do
   describe "GET /index" do
-
     it "assigns @wines" do
-      wine = Wine.create(name: "Casillero del diablo")
+      Wine.last.delete
+      @user = User.create(email: "ed@gmail.com", password: "123456")
+      sign_in @user
+      wine = Wine.create(name: "Test wine")
       get "/wines"
       expect(assigns(:wines)).to eq([wine])
     end
 
     it "renders the index template" do
+      @user = User.create(email: "ed@gmail.com", password: "123456")
+      sign_in @user
       get "/wines"
       expect(response).to render_template(:index)
     end
   end
 
   describe "GET /show" do
-
     it "renders the show template" do
-      get "/wines/:id"
+      @user = User.create(email: "ed@gmail.com", password: "123456")
+      sign_in @user
+      wine = Wine.create(name: "Test wine")
+      get "/wines/#{wine.id}"
       expect(response).to render_template(:show)
     end
   end
